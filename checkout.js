0e23a38da0c8424ec6973e9842b644688f735e71
MJ AHMAD <167817895+MJ-AHMAD@users.noreply.github.com>
2024-07-08 09:29:30 -0700

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,6 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = Stripe(
+  'pk_test_51PZzciCyC8OWGlHKRThmLWoQvX29CC6L8d6ExLaM6xGYdXSAQ6ksP4yxqKRLHLvNIIw2OdweUNOe2iNXpCHpXH7W00k6g9ZBKG',
+  {betas: ['custom_checkout_beta_2']},
+);
