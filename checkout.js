847210711f9fee8d75123a849a5b602198e3927f
MJ AHMAD <167817895+MJ-AHMAD@users.noreply.github.com>
2024-07-08 09:30:05 -0700

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -4,3 +4,6 @@ const stripe = Stripe(
   'pk_test_51PZzciCyC8OWGlHKRThmLWoQvX29CC6L8d6ExLaM6xGYdXSAQ6ksP4yxqKRLHLvNIIw2OdweUNOe2iNXpCHpXH7W00k6g9ZBKG',
   {betas: ['custom_checkout_beta_2']},
 );
+stripe.initCustomCheckout({clientSecret}).then((checkout) => {
+  console.log(checkout);
+});
